6eaf07f1cce5df7a324fd4fbfe557ab9e8f2e0be
Mikael Ravantti <Mikael.Ravantti@gmail.com>
2018-10-17 11:07:21 +0200

    Added .editorconfig, stylesheet and updated index php with link to stylesheet

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ declare(strict_types=1);
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="assets/stylesheet/main.css">
         <title></title>
     </head>
     <body>
