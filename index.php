ee5f4652a6904a85c7d09f0c520bad864ef69622
Mikael Ravantti <Mikael.Ravantti@gmail.com>
2018-10-24 21:30:48 +0200

    Cleaned up the code and added comments to areas needed

--- index.php
+++ index.php
@@ -16,52 +16,83 @@ require __DIR__. '/functions.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="assets/stylesheet/main.css">
-        <title>Fake News</title>
-    </head>
-	 <header>
-		 <h1 class="logo"><a href="#">Fake News</a></h1>
-	 </header>
-	 <nav class="nav">
-	   <ul>
-			<li><a href="#">Hodor</a></li>
-			<li><a href="#">Bruce Wayne</a></li>
-			<li><a href="#">Rick Sanchez</a></li>
-			<li><a href="#">Bane</a></li>
-			<li><a href="#">Emmett Lathrop "Doc" Brown</a></li>
-	   </ul>
-	 </nav>
-    <body>
-		 <div class="container">
-		 <?php foreach ($data['articles'] as $articles): ?>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="assets/stylesheet/main.css">
+	<title>Fake News</title>
+</head>
 
-		 <article style="background-color: rgba(<?= $articles['backgroundColor'] ?>);">
-			 <div class="articleContainer">
+<!-- Logo -->
 
-				 <h1 class="title"><?= "$articles[title]" ?> </h1>
+<header>
+	<h1 class="logo"><a href="index.php">Fake News</a></h1>
+</header>
 
-				 <p class="contentText"><?= "$articles[content]" ?> </p><br><br>
+<!-- Navigation -->
 
-				 <p><span class="publishedSpan">Published: </span> <?= "$articles[publishedDate] "?><span class="likesSpan">Likes: </span> <?="$articles[likeCounter]"; ?></p>
-			 </div>
-				 <div class="authorContainer">
-				<?php foreach ($data['authors'] as $author): ?>
+<nav class="nav">
+	<ul>
+		<li><a href="#">Hodor</a></li>
+		<li><a href="#">Bruce Wayne</a></li>
+		<li><a href="#">Rick Sanchez</a></li>
+		<li><a href="#">Bane</a></li>
+		<li><a href="#">Emmett Lathrop "Doc" Brown</a></li>
+	</ul>
+</nav>
 
+<body>
+
+	<!-- Container for all the articles -->
+
+	<div class="container">
+
+		<!-- Foreach loop to print out the articles -->
+
+		<?php foreach ($data['articles'] as $articles): ?>
+
+			<article style="background-color: rgba(<?= $articles['backgroundColor'] ?>);">
+
+				<!-- Container for each single article -->
+
+				<div class="articleContainer">
+
+					<h1 class="title"><?= "$articles[title]" ?> </h1>
+
+					<p class="contentText"><?= "$articles[content]" ?> </p>
+
+					<p>
+						<span class="publishedSpan">Published: </span>
+						<?= "$articles[publishedDate] "?>
+						<span class="likesSpan">Likes: </span>
+						<?="$articles[likeCounter]"; ?>
+					</p>
+
+				</div>
+
+				<!-- Container for the author info -->
+
+				<div class="authorContainer">
+
+					<!-- Foreach loop to print out the author names -->
+
+					<?php foreach ($data['authors'] as $author): ?>
+
+						<!-- If statement that check the author id and print the corresponding name and picture -->
 
 						<?php if ($articles['author'] === $author['id']): ?>
 
 							<a href="#"><img class="img" src="<?= $author['image'] ?>" alt=""></a>
 
-							<p class="authorInfo"><span class="authorSpan">Author: </span><br> <a class="authorLink" href="#"><?= "$author[name]"; ?></a></p>
-
+							<p class="authorInfo">
+								<span class="authorSpan">Author: </span>
+								<br> <a class="authorLink" href="#"><?= "$author[name]"; ?></a>
+							</p>
 
 						</div>
-				<?php endif; endforeach; ?>
-		 </article>
-	 <?php endforeach; ?>
- </div>
-    </body>
-</html>
+					<?php endif; endforeach; ?>
+				</article>
+			<?php endforeach; ?>
+		</div>
+	</body>
+	</html>
